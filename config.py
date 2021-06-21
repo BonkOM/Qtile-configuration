15d2339fd7e4adf487daac46d1ef3dca551a260b
BonkOM <86239419+BonkOM@users.noreply.github.com>
2021-06-21 09:02:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget, extension
@@ -214,78 +188,6 @@ screens = [
                 #widget.QuickExit(),
             ],
             40,
-            background="#003041",
-            #opacity=0.5,
-        ),
-    ),
-    Screen(
-        top=bar.Bar(
-            [
-                #widget.CurrentLayout(),
-                #widget.GroupBox(
-                    #border="#FFE2A8",
-                    #background="#003041",
-                    #highlight_color=["#FFAA00", "#FFE2A8"],
-                    #highlight_method='line',
-                    #hide_unused=True,
-                    #),
-                #widget.Prompt(),
-                widget.WindowName(),
-                #widget.Systray(),
-                #widget.TextBox("|"),
-                widget.TextBox("Volumen"),
-                widget.Volume(),
-                widget.TextBox("|"),
-                widget.Clock(
-                    format='%a',
-                    fontsize=14,
-                    ),
-                widget.TextBox("|"),
-                widget.Clock(format='%d-%m-%Y'),
-                widget.TextBox("|"),
-                widget.Clock(
-                    format='%I:%M %p',
-                    fontsize=16,
-                    ),
-                #widget.QuickExit(),
-            ],
-            24,
-            background="#003041",
-            #opacity=0.5,
-        ),
-    ),
-    Screen(
-        top=bar.Bar(
-            [
-                #widget.CurrentLayout(),
-                #widget.GroupBox(
-                    #border="#FFE2A8",
-                    #background="#003041",
-                    #highlight_color=["#FFAA00", "#FFE2A8"],
-                    #highlight_method='line',
-                    #hide_unused=True,
-                    #),
-                #widget.Prompt(),
-                widget.WindowName(),
-                #widget.Systray(),
-                #widget.TextBox("|"),
-                widget.TextBox("Volumen"),
-                widget.Volume(),
-                widget.TextBox("|"),
-                widget.Clock(
-                    format='%a',
-                    fontsize=14,
-                    ),
-                widget.TextBox("|"),
-                widget.Clock(format='%d-%m-%Y'),
-                widget.TextBox("|"),
-                widget.Clock(
-                    format='%I:%M %p',
-                    fontsize=16,
-                    ),
-                #widget.QuickExit(),
-            ],
-            24,
             background="#003041",
             #opacity=0.5,
         ),
